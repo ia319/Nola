222a0ded5726422b80b7490c07ae922196ffac42
ia <linglingmeng876@gmail.com>
2026-02-22 16:44:03 +0800

    feat(config): add supported language codes constant

--- core/nola/config/__init__.py
+++ core/nola/config/__init__.py
@@ -1,11 +1,16 @@
 """Configuration module."""
 
-from nola.config.constants import ALLOWED_AUDIO_TYPES, ALLOWED_EXTENSIONS
+from nola.config.constants import (
+    ALLOWED_AUDIO_TYPES,
+    ALLOWED_EXTENSIONS,
+    SUPPORTED_LANGUAGES,
+)
 from nola.config.settings import Settings, settings
 
 __all__ = [
     "ALLOWED_AUDIO_TYPES",
     "ALLOWED_EXTENSIONS",
+    "SUPPORTED_LANGUAGES",
     "Settings",
     "settings",
 ]
