9ab95df07f469a1d152a2d62b7749cffd9b491a5
ia <linglingmeng876@gmail.com>
2026-03-22 18:28:04 +0800

    feat(export): extend export api for defaults and filename
    - add export config read and defaults mutation apis
    - parse batch download filename from content-disposition
    - expose single export filename and target request options

--- app/src/features/export/index.ts
+++ app/src/features/export/index.ts
@@ -1,1 +1,19 @@
-export {}
+export {
+  batchExport,
+  deleteExportDefaults,
+  downloadExport,
+  fetchExportConfig,
+  patchExportDefaults,
+  saveExport,
+} from './api'
+export type {
+  BatchExportDownloadResult,
+  ExportRequestOptions,
+  SingleExportApiOptions,
+  SingleExportRequestOptions,
+  SingleExportTarget,
+} from './api'
+export { ExportDialog } from './components/ExportDialog'
+export type { ExportDialogValue } from './components/ExportDialog'
+export { useExportDefaults } from './hooks/useExportDefaults'
+export { buildSingleExportFilename } from './lib/filename'
